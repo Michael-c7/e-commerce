bade06f26a6394bc85f8a1bf7da87638b3300de0
Michael Carr <mcarr2468@gmail.com>
2021-07-09 18:05:28 -0500

    V1.2 added more to the starter html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ https://apilist.fun/category/ecommerce
 
 
 let test = async _ => {
-    let endPoint = "https://fakestoreapi.com/products/1";
+    let endPoint = "https://fakestoreapi.com/products/categories";
 
     try {
         let request = await fetch(endPoint);
@@ -19,4 +19,50 @@ let test = async _ => {
 
 }
 
-test()
+// test()
+
+
+/*
+<button class="header__filter-search">
+                <i class="fa fa-sort" aria-hidden="true"></i>
+
+                <div class="filter-content">
+                    <button class="filter-content__clear-filter">Clear Filter</button>
+
+                    <div class="filter-content__products">
+                        <div>
+                            <button class="filter-content__products-btn">
+                                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                            </button>
+                            <div>Product type</div>
+                        </div>
+                        <form action="">
+                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                                <label for="vehicle1"> I have a bike</label>
+                                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                                <label for="vehicle2"> I have a car</label>
+                                <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+                                <label for="vehicle3"> I have a boat</label>
+                        </form>
+                    </div>
+
+                    <div class="filter-content__price">
+                        <div>
+                            <button class="filter-content__price-btn">
+                                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                            </button>
+                            <div>Price type</div>
+                        </div>
+                        <form action="">
+                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                                <label for="vehicle1"> I have a bike</label>
+                                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                                <label for="vehicle2"> I have a car</label>
+                                <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+                                <label for="vehicle3"> I have a boat</label>
+                        </form>
+                    </div>
+                </div>
+            </button>
+
+*/
