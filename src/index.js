0b18cc2495ad41e6849e245340fb103ea9e06221
Michael Carr <mcarr2468@gmail.com>
2021-07-09 15:17:27 -0500

    V1.1 added starting html & search bar css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.scss';
+/*
+https://fakestoreapi.com/
+
+https://apilist.fun/category/ecommerce
+*/
+
+
+let test = async _ => {
+    let endPoint = "https://fakestoreapi.com/products/1";
+
+    try {
+        let request = await fetch(endPoint);
+        let data = await request.json();
+        console.log(data)
+    }catch(err) {
+        console.log(err);
+    }
+
+}
+
+test()
