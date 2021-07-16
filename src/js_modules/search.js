5262f04dc73ac81b0dce989ddce1f3ef4d6d841e
Michael Carr <mcarr2468@gmail.com>
2021-07-15 19:11:25 -0500

    V1.53 get the users inputs

--- src/js_modules/search.js
+++ src/js_modules/search.js
@@ -1,19 +1,31 @@
-// import { productItem } from "./js_modules/productItems";
+import { productItem } from "./productItems";
 
 const searchBarEl = document.querySelector(".search-bar");
+const searchBarBtnEl = document.querySelector(".search-bar__submit");
+const searchBarInputEl = document.querySelector(".search-bar__input");
 
+let userInput = "";
 
 export const searchProducts = _ => {
     console.log("the search module");
+    productItem()
 }
 
 
-searchBarEl.addEventListener("submit", event => {
-    console.log(event)
-    searchProducts()
-});
 
 
-export const updateProducts = _ => {
-    // productItem()
+
+const getUserInput = _ => {
+    searchBarBtnEl.addEventListener("click", event => {
+        event.preventDefault();
+        if(searchBarInputEl === "") return;
+        userInput = searchBarInputEl.value;
+        console.log(userInput)
+    });
 }
+
+getUserInput()
+
+
+
+/*have to search / filter locally*/
